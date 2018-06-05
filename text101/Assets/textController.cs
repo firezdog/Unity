14dc4bb8689b789aeb1e150928f1adea885ef08c
Alex <firezdog@yahoo.com>
2018-06-04 21:20:02 -0500

    finished basic game / proof of concept

--- text101/Assets/textController.cs
+++ text101/Assets/textController.cs
@@ -21,6 +21,9 @@ public class textController : MonoBehaviour {
 		States.Add("cell_mirror", state_cell_mirror);
 		States.Add("sheets_1", state_sheets_1);
 		States.Add("lock_1", state_lock_1);
+		States.Add("code_alarm", state_code_alarm);
+		States.Add("examine_keys_closely", state_examine_keys_closely);
+		States.Add("outside_cell", state_outside_cell);
 		myState = "cell";
 	}
 	
@@ -31,9 +34,48 @@ public class textController : MonoBehaviour {
 
 	}
 
+	void state_outside_cell() {
+		text.text = "You've escaped the cell...into a hallway filled with guards. You could face many quandaries and pitfalls as you navigated your way through the hall and out of the prison to the Great Wide World...but this application doesn't scale well, and I think I've learned enough about the basics of Unity to move on to a new assignment.";
+	}
+
+	void state_examine_keys_closely(){
+		text.text = "You peer into the mirror and scrutinize the fingerprints.  The fingerprint on \'3\' appears smudged, as if it were pressed several times, rapidly. The fingerprint on \'4\' blends in with the fingerprint on \'3\'.\n\nTo guess \"3466\", press A. To guess \"3364\", press B. To guess \"3346\", press C.";
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            myState = "code_alarm";
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            myState = "code_alarm";
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            myState = "outside_cell";
+        }
+
+	}
+
+	void state_code_alarm() {
+		text.text = "You punch in the keys and hear a click. You push on the prison cell, but it remains closed. An alarm sounds and you hear the approach of heavy footsteps.  It appears that in your haste to escape, you've alerted the guards.  You should have thought twice before entering a code...\n\nGame Over! To restart, press R.";
+
+		if(Input.GetKeyDown(KeyCode.R)){
+			myState = "cell";
+		}
+	}
+
 	void state_lock_1() {
 
-		text.text = "By holding the mirror close to the keypad, you are able to make out the smudge of the guard's fingerprints on numbers 3, 3, and 6.\n\nTo go back to the cell, press C. To enter the keycode \"336\", press K. To examine the lock more closely, press E.";
+		text.text = "By holding the mirror close to the keypad, you are able to make out the smudge of the guard's fingerprints on numbers 3, 4, and 6.\n\nTo go back to the cell, press C. To enter the keycode \"346\", press K. To examine the lock more closely, press E.";
+
+		if(Input.GetKeyDown(KeyCode.K)){
+			myState = "code_alarm";
+		} else if(Input.GetKeyDown(KeyCode.E)) {
+			myState = "examine_keys_closely";
+		} else if(Input.GetKeyDown(KeyCode.C)) {
+			myState = "cell_mirror";
+		}
+
 
 	}
 
